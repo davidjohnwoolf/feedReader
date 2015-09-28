37d383ec345861f20b1cfb82768152faee1002f4
David Woolf <davidjohnwoolf@gmail.com>
2015-09-27 18:07:25 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.disable('x-powered-by');
+app.set('port', process.env.PORT || 1337);
+
+mongoose.connect('mongodb://localhost/spa');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+// error handling
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Page Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(app.get('port'));
+console.log('Listening...');
