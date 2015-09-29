5e17dfc2472ff2051f792aec95c45215798afe8c
David Woolf <davidjohnwoolf@gmail.com>
2015-09-28 22:10:34 -0700

    create feeds controller

---
+++ controllers/feeds.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var methodOverride = require('method-override');
+
+var Feed = require('../models/feed.js')
+
+// router middleware
+router.use(methodOverride(function(req, res) {
+  // check for _method property in form requests
+  // see hidden input field in views
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
